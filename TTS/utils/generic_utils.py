455ba5e60b7b7852b1cfa04a6d9459f7107b8844
Omar Ghoneim <omyg95@gmail.com>
2019-04-15 16:35:47 +0200

    Add files via upload

--- TTS/utils/generic_utils.py
+++ TTS/utils/generic_utils.py
@@ -10,3 +10,16 @@ def load_config(config_path):
     config = AttrDict()
     config.update(json.load(open(config_path, "r")))
     return config
+
+
+def sequence_mask(sequence_length, max_len=None):
+    if max_len is None:
+        max_len = sequence_length.data.max()
+    batch_size = sequence_length.size(0)
+    seq_range = torch.arange(0, max_len).long()
+    seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
+    if sequence_length.is_cuda:
+        seq_range_expand = seq_range_expand.cuda()
+    seq_length_expand = (sequence_length.unsqueeze(1)
+                         .expand_as(seq_range_expand))
+    return seq_range_expand < seq_length_expand
